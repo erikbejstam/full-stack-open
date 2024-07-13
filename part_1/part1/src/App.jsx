c3d7d524fd72e81a827df4386ff3b38337fd68aa
Erik Bejstam <erikbejstam@hotmail.com>
2024-07-13 23:03:57 +0200

    Solve part 1.3

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -18,7 +18,7 @@ const Content = (props) => {
 const Part = (props) => {
   console.log(props)
   return(
-    <p>{props.part.part} {props.part.exercises}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
@@ -32,11 +32,19 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {part: 'Fundamentals of React', exercises: 10},
-    {part: 'Using props to pass data', exercises: 7},
-    {part: 'State of a component', exercises: 14}
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const parts = [part1, part2, part3]
 
   return (
     <div>
